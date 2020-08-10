49e526a6b49c2f89e63b78ea1e345b7ea8d95065
github username <github email address>
2020-08-10 11:21:05 -0500

    Done

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,9 +1,49 @@
 class Company
   attr_reader :name, :founding_year
-
+  
+  @@all = []
+  
   def initialize(name, founding_year)
     @name = name
     @founding_year = founding_year
+
+    @@all << self
   end
+#returns an array of all the freebies given out by the company
+  def freebie_company
+    Freebie.all.select do |freebie_instance|
+      freebie_instance.company == self
+    end 
+  end 
+
+  #Company#give_freebie(dev, item_name)
+#takes a dev (an instance of the Dev class) and an item_name (string) as arguments, and creates a new Freebie instance associated with this company and the given dev
+
+  def give_freebie(dev, item_name)
+    Freebie.new(dev, item_name, self)
+  end 
+#returns the Company instance with the earliest founding year
+  def self.oldest_company
+    oldest = []
+    self.all.find do |company_instance|
+      oldest << company_instance.founding_year.min
+    end 
+    oldest
+  end 
+  
+  
+  
+  def self.all
+    @@all
+  end 
+
+
+
+
+
+
+
+
+
 
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,8 +1,68 @@
 class Dev
   attr_reader :name
-
+  @@all = []
   def initialize(name)
     @name = name
+
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end 
+#returns a unique array of all the devs who received freebie(s) from the company
+  def dev
+    Freebie.all.select do |dev_instance|
+      dev_instance.item_name == self
+      binding.pry
+    end 
+  end 
+
+  def dev_freebies
+    Freebie.all.select do |freebie_instance|
+        freebie_instance.dev == self
+        binding.pry
+    end 
+  end 
+
+  #should return the Dev instance for this freebie
+  def dev_instance
+    Freebie.all.find do |freebie_instance|
+      freebie_instance.dev == self
+    end 
+  end 
+
+  #accepts an item_name (string) and returns true if any of the freebies associated #with the dev has that item_name, otherwise returns false
+  def received_one?(item_name)
+    Freebie.all.map do |freebie_instance|
+      if freebie_instance.dev == self
+        return true
+      else
+        return false
+        binding.pry
+      end 
+    end 
+  end 
+#accepts a Dev instance and a Freebie instance, changes the freebie's dev to be the given dev
+  def give_away(dev, freebie)
+    Freebie.all.find do |freebie_instance|
+      freebie_instance.dev == self
+      binding.pry
+    end 
+  end 
+
+  #returns a unique array of all the companies that the dev received freebie(s) from
+  def dev_companies
+    Freebie.all.select do |freebie_instance|
+      freebie_instance.company == self
+    end 
+  end 
+
+  #returns one dev instance for the dev who owns the most amount of freebies
+  def self.hoarder
+    Freebie.all.map do |freebie_instance|
+      freebie_instance
+    end 
+  end 
 
 end
