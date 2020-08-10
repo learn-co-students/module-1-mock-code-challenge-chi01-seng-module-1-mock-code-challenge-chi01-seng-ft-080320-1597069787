9479cd35a635d543bfb5a07fdf701d3b19133462
github username <github email address>
2020-08-10 11:24:41 -0500

    completed to my fullest knowledge

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,9 +1,26 @@
 class Company
   attr_reader :name, :founding_year
 
+    @@all = []
+
   def initialize(name, founding_year)
     @name = name
     @founding_year = founding_year
+    @@all << self 
   end
+
+  def self.all 
+    @@all 
+  end 
+
+  def freebies
+    Freebie.all.select {|freebie| freebie.company == self }
+  end 
+
+  def devs 
+    Freebie.all.uniq {|freebie| freebie.company == self} 
+  end 
+
+
 
 end
--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,26 @@
 class Freebie
 
+    attr_accessor :item_name, :company, :dev
+
+    @@all = []
+
+    def initialize(item_name, company, dev)
+        @item_name = item_name
+        @company = company
+        @dev =  dev
+        @@all << self
+    end
+
+    def item_name 
+        @item_name
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def dev
+        
+    end 
+
 end
