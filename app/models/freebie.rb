59aea612e0a5cd6681e492a9e1835d95b17ca64d
github username <hector.bale11@gmail.com>
2020-08-10 11:29:39 -0500

    Done

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,6 @@
 class Freebie
-
+  def initialize(item_name, company, dev)
+    @item_name = item_name 
+    @company = company
+    @dev = dev 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -10,6 +10,23 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+class Company
+  attr_reader :name, :founding_year 
+  @@all = []
+
+  def initialize(name, founding_year)
+    @name = name
+    @founding_year = founding_year
+    @@all << self 
+  end
+
+  def self.all 
+    @@all 
+ end 
+
+end
+
+google = Company.new("google", 1998)
 
 
 
