fe03fc21c9646ead5ae42fc6feb1029367d7a1d3
KRStoddard <kimberlynrstoddard@gmail.com>
2020-08-10 10:36:44 -0500

    Core deliverables done

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,8 +1,44 @@
 class Dev
+
+  #initialize with name
+  #keep track of all instances of dev
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+
+    self.class.all << self 
   end
 
-end
+  def self.all 
+    @@all 
+  end
+
+  #instance method that returns array of all freebies given to dev
+  def freebies
+    Freebie.all.select {|freebie_instance| freebie_instance.dev == self}
+  end
+
+  #instance method that returns unique array of companies that gave freebie
+  def companies 
+    company = self.freebies.map {|freebie| freebie.company}
+    company.uniq 
+  end
+
+  #instance method to see if they received freebie
+  def received_one?(item_name)
+    self.freebies.map {|freebie| freebie.item_name == item_name}
+  end
+
+  #instance method for one dev to give another their freebie
+  def give_away(dev, freebie)
+    freebie.dev = dev 
+  end
+
+  #class constructor finds dev who owns most amount of freebies
+  def self.freebie_hoarder 
+    self.all.max_by {|dev_instance| dev_instance.freebies.count}
+  end
+
+end #of Dev class
--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,27 @@
 class Freebie
 
-end
+    #initialize with argument item_name, company, and dev
+    #keep track of all instances of Freebie
+
+    def initialize(item_name, company, dev)
+        @item_name = item_name
+        @company = company
+        @dev = dev 
+
+        self.class.all << self
+    end
+
+    @@all = []
+    attr_reader :item_name, :company
+    attr_accessor :dev  
+
+    def self.all
+        @@all 
+    end
+
+    #instance method to print details of freebie
+    def print_details
+        p "#{@dev.name} owns a #{@item_name} from #{@company.name}"
+    end
+
+end #of Freebie class
